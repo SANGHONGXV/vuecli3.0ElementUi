ef5ac4a8edce9fe449de3a9ae2f414ba6f653c1a
桑洪旭 <sanghongxv@163.com>
2019-07-23 15:37:53 +0800

    添加实例

--- src/router.js
+++ src/router.js
@@ -1,25 +1,53 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import NotFound from './views/404.vue'
 import Home from './views/Home.vue'
+import Login from './views/Login.vue'
+import Overview from '@/components/overview.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
 
 Vue.use(Router)
 
-export default new Router({
+let routes= new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
+    { 
+      path: '/login',
+      name: 'login',
+      component: Login,
+      hidden: true
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/',
+      component: Home,
+      name: '',
+      iconCls: 'el-icon-message',//图标样式class
+      leaf: true,//只有一个节点
+      children: [
+          { path: '/overview', component: Overview, name: '概览'},
+      ]
+    },
+    {
+      path: '/',
+      component: Home,
+      name: '导航一',
+      children: [
+          { path: '/helloWorld', component: HelloWorld, name: 'HelloWorld'},
+      ]
+    },
+    {
+      path: '/404',
+      name: '404',
+      component: NotFound,
+      hidden: true
+    },
+    {
+      path: '*',
+      hidden: true,
+      redirect: { path: '/404' }
     }
   ]
 })
+
+export default routes;
