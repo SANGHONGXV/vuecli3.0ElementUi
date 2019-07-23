5c65610ab593c81f10f766908086dffc26612a06
桑洪旭 <sanghongxv@163.com>
2019-07-23 12:02:35 +0800

    no commit message

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{this.data}}
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import * as test from '@/api/test'
+export default {
+  name: 'home',
+  data() {
+    return {
+      data:{}
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  mounted() {
+    test.test().then(res => {
+      this.data = res
+    })
+  }
+}
+</script>
